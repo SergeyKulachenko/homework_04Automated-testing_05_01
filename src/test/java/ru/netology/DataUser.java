9e1ca0ec92126c844286a9d204d55bc04a6f4765
Sergey K <89184083452.mail.ru>
2020-11-11 04:04:28 +0300

    правки

--- src/test/java/ru/netology/DataUser.java
+++ src/test/java/ru/netology/DataUser.java
@@ -1,26 +1,60 @@
 package ru.netology;
 
+import com.codeborne.selenide.SelenideElement;
 import com.github.javafaker.Faker;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.openqa.selenium.Keys;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+import java.util.Random;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 @Data
 @RequiredArgsConstructor
 public class DataUser {
-    private String name;
-    private String phone;
-
     Faker faker = new Faker(new Locale("ru"));
 
-    public String dataName() {
-        name = faker.name().fullName();
-        return name;
+    @Value
+    public static class VerifData {
+        private String name;
+        private String phone;
     }
 
-    public String dataPhone() {
-        phone = faker.phoneNumber().phoneNumber();
-        return phone;
+    void cityNoVal() {
+        String cityNoVal = faker.address().city();
+        $("[placeholder=Город]").setValue(cityNoVal);
     }
+
+    void cityInput() {
+        Random random = new Random();
+        $("[placeholder=Город]").setValue("Кр");
+        var size = $$("[class=popup__content] .menu-item__control").size();
+        int rand = random.nextInt(size);
+        $$("[class=popup__content] .menu-item__control").get(rand).click();
+    }
+
+    void dataNamePhone() {
+
+        String name = faker.name().lastName();
+        name = name + " " + faker.name().firstName();
+        $("[data-test-id=name].input_type_text .input__control").setValue(name);
+        String phone = faker.phoneNumber().phoneNumber();
+        $("[data-test-id=phone]").$("[name=phone]").setValue(phone);
+    }
+
+
+    public String dataInput(int days) {
+        String inputDate = LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.uuuu"));
+        SelenideElement data = $("[data-test-id=date]");
+        data.$("[value]").doubleClick().sendKeys(Keys.BACK_SPACE);
+        data.$("[placeholder]").setValue(inputDate);
+        return inputDate;
+    }
+
 }
--- src/test/java/ru/netology/Test.java
+++ src/test/java/ru/netology/Test.java
@@ -1,19 +1,14 @@
 package ru.netology;
 
 import com.codeborne.selenide.SelenideElement;
-import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.Keys;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
 public class Test {
-    private Faker faker;
     DataUser dataUser = new DataUser();
 
     @BeforeEach
@@ -21,40 +16,27 @@ public class Test {
         open("http://localhost:9999");
     }
 
-    public String dataInput() {
-        String inputDate = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.uuuu"));
-        SelenideElement data = $("[data-test-id=date]");
-        data.$("[value]").doubleClick().sendKeys(Keys.BACK_SPACE);
-        data.$("[placeholder]").setValue(inputDate);
-        return inputDate;
-    }
 
     @org.junit.jupiter.api.Test
     void getTrueInputValidForm() throws InterruptedException {
-        $("[placeholder=Город]").setValue("Кр");
-        $$("[class=popup__content] .menu-item__control").last().click();
-        dataInput();
-        String name = dataUser.dataName();
-        $("[data-test-id=name].input_type_text .input__control").setValue(name);
-        String phone = dataUser.dataPhone();
-        $("[data-test-id=phone]").$("[name=phone]").setValue(phone);
+        dataUser.cityInput();
+        dataUser.dataInput(6);
+        dataUser.dataNamePhone();
         $("[class=checkbox__box]").click();
         $$("[class=button__text]").find(exactText("Запланировать")).click();
-        Thread.sleep(1500);
+        $("[data-test-id=success-notification]").$("[class=notification__content]")
+                .shouldHave(textCaseSensitive("Встреча успешно запланирована на " + dataUser.dataInput(6)));
         $$("[class=button__text]").find(exactText("Запланировать")).click();
         $$("[class=button__text]").find(exactText("Перепланировать")).click();
         $("[data-test-id=success-notification]").$("[class=notification__content]")
-                .shouldHave(textCaseSensitive("Встреча успешно запланирована на " + dataInput()));
+                .shouldHave(textCaseSensitive("Встреча успешно запланирована на " + dataUser.dataInput(6)));
     }
 
     @org.junit.jupiter.api.Test
     void errorExpectedWhenInputIncorrectCity() {
-        $("[placeholder=Город]").setValue("Сочи");
-        dataInput();
-        String name = dataUser.dataName();
-        $("[data-test-id=name].input_type_text .input__control").setValue(name);
-        String phone = dataUser.dataPhone();
-        $("[data-test-id=phone]").$("[name=phone]").setValue(phone);
+        dataUser.cityNoVal();
+        dataUser.dataInput(6);
+        dataUser.dataNamePhone();
         $("[class=checkbox__box]").click();
         $$("[class=button__text]").find(exactText("Запланировать")).click();
         $("[data-test-id=city] .input__sub").shouldHave
@@ -63,13 +45,10 @@ public class Test {
 
     @org.junit.jupiter.api.Test
     void errorExpectedWhenEmptyFieldDate() {
-        $("[placeholder=Город]").setValue("Краснодар");
+        dataUser.cityInput();
         SelenideElement data = $("[data-test-id=date]");
         data.$("[value]").doubleClick().sendKeys(Keys.BACK_SPACE);
-        String name = dataUser.dataName();
-        $("[data-test-id=name].input_type_text .input__control").setValue(name);
-        String phone = dataUser.dataPhone();
-        $("[data-test-id=phone]").$("[name=phone]").setValue(phone);
+        dataUser.dataNamePhone();
         $("[class=checkbox__box]").click();
         $$("[class=button__text]").find(exactText("Запланировать")).click();
         $("[data-test-id=date] .input__sub").shouldHave
