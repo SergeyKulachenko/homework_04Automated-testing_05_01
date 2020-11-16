c83061a2587f5328e5ef8a1dbd0df416fe82ce29
Sergey K <89184083452.mail.ru>
2020-11-16 23:38:12 +0300

    правки 2

--- src/test/java/ru/netology/DataUser.java
+++ src/test/java/ru/netology/DataUser.java
@@ -2,8 +2,6 @@ package ru.netology;
 
 import com.codeborne.selenide.SelenideElement;
 import com.github.javafaker.Faker;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import org.openqa.selenium.Keys;
 
@@ -15,46 +13,64 @@ import java.util.Random;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
-@Data
-@RequiredArgsConstructor
-public class DataUser {
-    Faker faker = new Faker(new Locale("ru"));
+class DataUser {
+    //    static Faker faker;
+    static Faker faker = new Faker(new Locale("ru"));
 
-    @Value
-    public static class VerifData {
-        private String name;
-        private String phone;
+    private DataUser() {
     }
 
-    void cityNoVal() {
-        String cityNoVal = faker.address().city();
-        $("[placeholder=Город]").setValue(cityNoVal);
-    }
-
-    void cityInput() {
+    //    @Value
+//    static class VerifData {
+//        private String name;
+//        private String phone;
+//
+//    }
+    static String cityForInput() {
         Random random = new Random();
-        $("[placeholder=Город]").setValue("Кр");
-        var size = $$("[class=popup__content] .menu-item__control").size();
-        int rand = random.nextInt(size);
-        $$("[class=popup__content] .menu-item__control").get(rand).click();
+        int rand = random.nextInt(12);
+        String city[] = {"Екатеринбург", "Йошкар-Ола", "Калининград", "Кемерово", "Киров", "Кострома", "Краснодар",
+                "Красноярск", "Курган", "Курск", "Санкт-Петербург", "Сыктывкар", "Чебоксары"};
+        return city[rand];
     }
 
-    void dataNamePhone() {
+    static String cityNoVal() {
+        Random random = new Random();
+        int rand = random.nextInt(12);
+        String cityNoVal[] = {"Мегион", "Кизляр", "Урус-Мартан", "Снежинск", "Кингисепп", "Заринск", "Курганинск"};
+        return cityNoVal[rand];
+    }
 
+
+    static String cityWishLetterEBrief() {
+        Random random = new Random();
+        int rand = random.nextInt(12);
+        String cityNoVal[] = {"Будёновск", "Кизляр", "Урус-Мартан", "Снежинск", "Кингисепп", "Заринск", "Курганинск"};
+        return cityNoVal[rand];
+    }
+
+    static String dataPhone() {
+        String phone = faker.phoneNumber().phoneNumber();
+        return phone;
+    }
+
+    static String dataName() {
         String name = faker.name().lastName();
         name = name + " " + faker.name().firstName();
-        $("[data-test-id=name].input_type_text .input__control").setValue(name);
-        String phone = faker.phoneNumber().phoneNumber();
-        $("[data-test-id=phone]").$("[name=phone]").setValue(phone);
+        return name;
     }
 
-
-    public String dataInput(int days) {
+    static String dataInput(int days) {
         String inputDate = LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.uuuu"));
-        SelenideElement data = $("[data-test-id=date]");
-        data.$("[value]").doubleClick().sendKeys(Keys.BACK_SPACE);
-        data.$("[placeholder]").setValue(inputDate);
         return inputDate;
     }
+//    void city_Input() {
+//        Random random = new Random();
+//        $("[placeholder=Город]").setValue("Кр");
+//        var size = $$("[class=popup__content] .menu-item__control").size();
+//        int rand = random.nextInt(size);
+//        $$("[class=popup__content] .menu-item__control").get(rand).click();
+//    }
+
 
 }
